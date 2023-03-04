b766e65dd942479fddf6ce13748e217e332265ad
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2023-03-03 21:52:41 -0500

    finish my project

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import "./App.css";
-import Card from "./components/card";
+import { useState } from "react";
 
 function App() {
+  const [myNumber, setMyNumber] = useState(0);
+
   return (
     <div className="App">
       <div className="gradient-blob blob-top" />
       <h1>
         React <span className="gradient-text">YRHacks</span> Workshop
       </h1>
-      <Card title="Hello" content="hi" />
-      <Card title="Hello" content="hi" />
-      <Card title="Hello" content="hi" />
+
+      <button
+        onClick={() => {
+          setMyNumber(myNumber + 1);
+          console.log(myNumber);
+        }}
+      >
+        Click Me! {myNumber}
+      </button>
     </div>
   );
 }
