6db70abfb48d131fb737883568b3d2da84c03631
ishaan1013 <69771365+ishaan1013@users.noreply.github.com>
2023-03-03 20:35:57 -0500

    init live

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="gradient-blob blob-top" />
+      <h1>
+        React <span className="gradient-text">YRHacks</span> Workshop
+      </h1>
+    </div>
+  );
+}
+
+export default App;
